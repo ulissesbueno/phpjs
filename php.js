9eb78aa14ff0b36ebda22c9ce56138ff8ee86476
Ulisses Bueno <has.ulisses.silva@jsl.com.br>
2017-03-06 14:17:27 -0300

    #

--- php.js
+++ php.js
@@ -1,9 +1,10 @@
 var $PHP = function( phpClass ) { 
 
-	this.server = '';
+	this.server = ( server ? server : '' );
 	this.class = phpClass;
 	this.params = "";
 	this.loaded = '';
+	this.url = '';
 	this.new = function( cls, callback ){
 
 						var me = this;
@@ -16,13 +17,6 @@ var $PHP = function( phpClass ) {
 								if( ret.success ){		
 
 									me.fillproperty( ret.data.attributes );
-
-									/*
-									for( var a in ret.data.methods ){
-										me[ret.data.methods[a]] = me.callmethod;
-									}
-									*/
-
 									if( me.loaded ) me.loaded();
 									if(callback) callback();
 								}
@@ -37,22 +31,19 @@ var $PHP = function( phpClass ) {
 	}
 
 	this.call = function( method, params, callback ){
-		this.callmethod( method, params, callback )
-	}
-
-	this.callmethod = function( method, params, callback ){
 		//var method = this.getMethod( arguments.callee.caller );
 		var me = this;
 		me.params = params;
+		this.url = this.server+"/"+this.class+'/'+method;
 		$.ajax({
-			url: this.server+"/"+this.class+'/'+method,
+			url: this.url,
 			dataType : "json",
 			data: JSON.stringify(me),
 			type: 'POST',
 			success : function( ret ){
 				me.fillproperty( ret.data.attributes );
 				if( ret.success ){
-					if(callback) callback( ret.data );	
+					if(callback) callback( ret );	
 				}
 				
 			}
